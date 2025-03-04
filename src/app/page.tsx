abd3bfa256edd769962706a88b72845c2eed6c6a
gifflet <guisousa09@hotmail.com>
2025-03-04 20:13:15 -0300

    feat: enable remote image loading and improve event listing
    - Added support for loading remote images from Freepik in 'next.config.ts'
    - Modified 'getEvents' function to disable cache for API calls
    - Replaced '<img>' with Next.js '<Image>' component for optimized image rendering
    - Adjusted event listing grid and improved event rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,28 @@ import { EventModel } from "@/models";
 import Image from "next/image";
 
 async function getEvents(): Promise<EventModel[]> {
-  return (await fetch('http://localhost:3000/api/events')).json()
+  return (await fetch('http://localhost:3000/api/events', {
+    cache: 'no-cache',
+  })).json()
 }
 
 export default async function Home() {
-  var events = await getEvents();
+  const events = await getEvents();
   return (
     <main className="container mx-auto py-8 px-4">
       <h1 className="text-3xl font-bold mb-8">Available Shows</h1>
       <div className="grid grid-cols-3 gap-8">
-        {events.map((event,key) => (
+        {events.map((event, key) => (
           <div key={key} className="bg-white shadow-lg rounded-lg">
-            <img className="w-full h-48 object-cover" src={event.image_url} alt={event.name} />
+            <Image className="w-full h-48 object-cover"
+              src={event.image_url}
+              alt={event.name}
+              width={300} height={200}/>
             <div className="p-4">
               <h2 className="text-xl text-black font-bold">{event.name}</h2>
               <p className="text-gray-500 mt-2"> {new Date(event.date).toLocaleDateString()}</p>
               <p className="text-gray-500 mt-2"> {event.available_spots} available spots</p>
-              <p className="text-gray-500 mt-2"> R$ {event.price}</p>
+              <p className="text-gray-500 mt-2"> $ {event.price}</p>
               <p>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded mt-4">Buy</button>
               </p>
